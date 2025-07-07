7a71dbe7f736732ef2749bbfdfbd7b46f6cbb8b7
reactten <reactnativeten@gmail.com>
2025-07-07 16:48:01 +0800

    first changes

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> and save to test HMR and we are checking for react
         </p>
       </div>
       <p className="read-the-docs">
